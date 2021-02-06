3f00e6887b56a7bf72650fcd26f57de65f5c7cc5
Vincent Hirtz <hirtzvincent@gmail.com>
2021-02-06 15:09:31 +0100

    add menu and contact page wip

--- components/intro.js
+++ components/intro.js
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import LinkedInIcon from "@material-ui/icons/LinkedIn";
 import FacebookIcon from "@material-ui/icons/Facebook";
 import InstagramIcon from "@material-ui/icons/Instagram";
@@ -8,7 +9,9 @@ export default function Intro() {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
       <h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
-        Vincent Hirtz
+        <Link href="/">
+          <a>Vincent Hirtz</a>
+        </Link>
       </h1>
       <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
         <a href="https://linkedin.com/in/hirtzvincent/" target="_blank">
--- components/layout.js
+++ components/layout.js
@@ -1,14 +1,100 @@
-import Alert from '../components/alert'
-import Footer from '../components/footer'
+import React, { useState } from "react";
+import Link from "next/link";
+import clsx from "clsx";
+import Alert from "../components/alert";
+import Footer from "../components/footer";
+import SwipeableDrawer from "@material-ui/core/SwipeableDrawer";
+import MenuIcon from "@material-ui/icons/Menu";
+import { makeStyles } from "@material-ui/core/styles";
+import List from "@material-ui/core/List";
+import Divider from "@material-ui/core/Divider";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import HomeIcon from "@material-ui/icons/Home";
+import MailOutlineIcon from "@material-ui/icons/MailOutline";
+
+const useStyles = makeStyles({
+  list: {
+    width: 250,
+  },
+  fullList: {
+    width: "auto",
+  },
+});
 
 export default function Layout({ preview, children }) {
+  const classes = useStyles();
+  const [open, setOpen] = useState(false);
+
+  const toggleDrawer = (bool) => {
+    console.log("bool ", bool);
+    setOpen(bool);
+  };
+
+  const list = (string) => (
+    <div
+      className={clsx(classes.list, {
+        [classes.fullList]: string === "top" || string === "bottom",
+      })}
+      role="presentation"
+      // onClick={() => toggleDrawer(false)}
+      // onKeyDown={() => toggleDrawer(false)}
+    >
+      <List>
+        <Link href="/">
+          <a>
+            <ListItem button key="Home">
+              <ListItemIcon>
+                <HomeIcon />
+              </ListItemIcon>
+              <ListItemText primary="Home" />
+            </ListItem>
+          </a>
+        </Link>
+        <Link href="/contact">
+          <a>
+            <ListItem button key="Contact">
+              <ListItemIcon>
+                <MailOutlineIcon />
+              </ListItemIcon>
+              <ListItemText primary="Contact" />
+            </ListItem>
+          </a>
+        </Link>
+      </List>
+      <Divider />
+      {/* <List>
+        {["All mail", "Trash", "Spam"].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemIcon>
+              {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+            </ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List> */}
+    </div>
+  );
+
   return (
     <>
       <div className="min-h-screen">
         <Alert preview={preview} />
+        <div style={{ padding: "20px 0 0 20px", cursor: "pointer" }}>
+          <MenuIcon onClick={() => toggleDrawer(true)} />
+          <SwipeableDrawer
+            anchor="left"
+            open={open === true}
+            onClose={() => toggleDrawer(false)}
+            onOpen={() => toggleDrawer(true)}
+          >
+            {list("left")}
+          </SwipeableDrawer>{" "}
+        </div>
         <main>{children}</main>
       </div>
       <Footer />
     </>
-  )
+  );
 }
