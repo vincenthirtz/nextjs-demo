a258c9b6a68be588823289c357b4b8e9d794f9de
Vincent Hirtz <vincent.hirtz@horoquartz.fr>
2021-02-08 10:39:26 +0100

    feat add description author html

--- components/post-header.js
+++ components/post-header.js
@@ -1,29 +1,34 @@
-import Avatar from '../components/avatar'
-import Date from '../components/date'
-import CoverImage from '../components/cover-image'
-import PostTitle from '../components/post-title'
+import Avatar from "../components/avatar";
+import Date from "../components/date";
+import CoverImage from "../components/cover-image";
+import PostTitle from "../components/post-title";
 
 export default function PostHeader({ titre, image, date, author }) {
   return (
     <>
       <PostTitle>{titre}</PostTitle>
       <div className="hidden md:block md:mb-12">
-      <Avatar alt={author.name} src={author.avatar} />
+        <Avatar alt={author.name} src={author.avatar} />
+        <div
+          style={{ margin: "10px" }}
+          dangerouslySetInnerHTML={{ __html: author.description }}
+        />
       </div>
       <div className="mb-8 md:mb-16 -mx-5 sm:mx-0">
-        <CoverImage
-          title={titre}
-          responsiveImage={image.responsiveImage}
-        />
+        <CoverImage title={titre} responsiveImage={image.responsiveImage} />
       </div>
       <div className="max-w-2xl mx-auto">
         <div className="block md:hidden mb-6">
-        <Avatar alt={author.name} src={author.avatar} />
+          <Avatar alt={author.name} src={author.avatar} />
+          <div
+            style={{ margin: "10px" }}
+            dangerouslySetInnerHTML={{ __html: author.description }}
+          />
         </div>
         <div className="mb-6 text-lg">
           <Date dateString={date} />
         </div>
       </div>
     </>
-  )
+  );
 }
