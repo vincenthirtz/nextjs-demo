a2891b3ddbc92177e399940c49cb8d8af7893eb4
Vincent Hirtz <vincent.hirtz@horoquartz.fr>
2021-02-08 11:54:54 +0100

    feat add meta head

--- pages/_document.js
+++ pages/_document.js
@@ -1,15 +1,21 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
 export default class MyDocument extends Document {
   render() {
     return (
-      <Html lang="en">
-        <Head />
+      <Html lang="fr">
+        <Head>
+          <title>Vincent Hirtz</title>
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
