889cf48b5ed974b8197b44140aded5aad30fe453
vincenthirtz <31092476+vincenthirtz@users.noreply.github.com>
2021-03-07 01:30:54 +0100

    migration done css

--- components/head.js
+++ components/head.js
@@ -2,17 +2,19 @@ import * as React from "react";
 import NextHead from "next/head";
 import { GoogleFonts } from "next-google-fonts";
 
-export const Head = () => (
-    <React.Fragment>
-        <GoogleFonts href="https://fonts.googleapis.com/css2?family=Rubik&display=swap" />
-        <NextHead>
-            <title>Vincent Hirtz</title>
-            <link rel="preconnect" href="https://fonts.gstatic.com" />
-            <meta
-                name="viewport"
-                content="initial-scale=1.0, width=device-width"
-            />
-            {children}
-        </NextHead>
-    </React.Fragment>
-);
+export default function Head() {
+    return (
+        <React.Fragment>
+            <GoogleFonts href="https://fonts.googleapis.com/css2?family=Rubik&display=swap" />
+            <NextHead>
+                <title>Vincent Hirtz</title>
+                <link rel="preconnect" href="https://fonts.gstatic.com" />
+                <meta
+                    name="viewport"
+                    content="initial-scale=1.0, width=device-width"
+                />
+                {children}
+            </NextHead>
+        </React.Fragment>
+    )
+}
