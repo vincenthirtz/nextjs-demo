95272b5ed660511386dcfcfc8a066acdd7923b77
vincenthirtz <31092476+vincenthirtz@users.noreply.github.com>
2021-03-11 21:28:11 +0100

    update css

--- components/head.js
+++ components/head.js
@@ -1,17 +1,26 @@
 import * as React from "react";
 import { Head } from "next/document";
-import { GoogleFonts } from "next-google-fonts";
 
 export default function CustomHead() {
     return (
         <React.Fragment>
-            <GoogleFonts href="https://fonts.googleapis.com/css2?family=Rubik&display=swap" />
             <Head>
                 <title>Vincent Hirtz</title>
-                <link rel="preconnect" href="https://fonts.gstatic.com" />
                 <meta
                     name="viewport"
                     content="initial-scale=1.0, width=device-width"
+                />
+                <link
+                    rel="preload"
+                    href="/fonts/Rubik-Regular.ttf"
+                    as="font"
+                    crossOrigin=""
+                />
+                <link
+                    rel="preload"
+                    href="/fonts/Rubik-Medium.ttf"
+                    as="font"
+                    crossOrigin=""
                 />
             </Head>
         </React.Fragment>
