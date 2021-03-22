f7be48aa70f31658bf7b79c2fa0f985d9f5af0f9
vincenthirtz <31092476+vincenthirtz@users.noreply.github.com>
2021-03-22 21:29:05 +0100

    test ok spotify

---
+++ pages/api/now-playing.js
@@ -0,0 +1,32 @@
+import { getNowPlaying } from '../../lib/spotify';
+var SpotifyWebApi = require('spotify-web-api-node');
+
+const client_id = process.env.NEXT_EXAMPLE_SPOTIFY_CLIENTID;
+const client_secret = process.env.NEXT_EXAMPLE_SPOTIFY_CLIENTSECRET;
+
+export default async (_, res) => {
+    const spotifyApi = new SpotifyWebApi({
+        clientId: client_id,
+        clientSecret: client_secret,
+        redirectUri: 'http://localhost:3000'
+    });
+
+    if (spotifyApi) {
+        spotifyApi.clientCredentialsGrant().then(data => {
+            // Save the access token so that it's used in future calls
+            spotifyApi.setAccessToken(data.body['access_token']);
+            spotifyApi.getPlaylist(
+                '13gGwH228n37VoIbY6NhbA',
+                { limit: 10, offset: 20 }, (err, data) => {
+                    if (err) {
+                        console.error('Something went wrong! ', err);
+                        return res.status(400).json(err)
+                    } else {
+                        return res.status(200).json(data.body)
+                    }
+                }
+            );
+
+        })
+    }
+};
