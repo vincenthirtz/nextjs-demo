76f1f8e5bea2fce2785c083b14caf1b05479f467
Vincent Hirtz <vincent.hirtz@horoquartz.fr>
2021-03-24 13:59:26 +0100

    add counter pageviews supabase fix text

--- components/PageViews/PageViews.js
+++ components/PageViews/PageViews.js
@@ -1,14 +1,22 @@
 import useSWR from "swr";
 
 const fetcher = async (req) => {
-    const res = await fetch(req);
-    return await res.json();
-  };
+  const res = await fetch(req);
+  return await res.json();
+};
 
 const PageViews = ({ slug }) => {
   const { data } = useSWR(`/api/views/${slug}`, fetcher);
 
-  return <>{data?.total ? `${data.total} vues` : `–––`}</>;
+  const getTextViews = () => {
+    if (data?.total === 1) {
+      return "vue";
+    } else if (data?.total > 1) {
+      return "vues";
+    }
+  };
+
+  return <>{data?.total ? `${data.total}  ${getTextViews()}` : `–––`}</>;
 };
 
 export default PageViews;
